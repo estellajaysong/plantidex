be179a3c80b354f43dde6133968574b73961a184
Estella Song <estellajaysong@gmail.com>
2020-09-02 21:16:50 -0700

    modify for my purposes

--- server/scripts/get_plant_images.py
+++ server/scripts/get_plant_images.py
@@ -1,19 +1,5 @@
 # Saved and modified from https://github.com/Msalmannasir/Google_image_scraper/blob/master/google_img.py
 
-# BC_native_plants = [
-#     'Aster',
-#     'Aquilegia formosa',
-#     # 'Sedum spathulifolium',
-#     # 'Delphinium menziesii',
-#     # 'Rosa nutkana',
-#     # 'Physocarpus capitatus'
-#     # 'Sambucus racemosa',
-#     # 'Gaultheria shallon',
-#     # 'Arbutus menziesii',
-#     # 'Cornus Eddie\'s White Wonder',
-#     # 'Sorbus aucuparia',
-#     # 'Acer circinatum'
-# ]
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
@@ -21,16 +7,20 @@ import requests
 import shutil
 import os
 
-user = os.getlogin()
-driver = driver = webdriver.Chrome(ChromeDriverManager().install())
-directory = '../images'
-inp = input("Enter what you wanna search?")
-url = (
-    'https://www.google.com/search?q='
-    + str(inp)
-    + '&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947'
-)
-iterate = int(input("Enter how many pics?"))
+BC_native_plants = [
+    'Aster',
+    'Aquilegia formosa',
+    'Sedum spathulifolium',
+    'Delphinium menziesii',
+    'Rosa nutkana',
+    'Physocarpus capitatus'
+    'Sambucus racemosa',
+    'Gaultheria shallon',
+    'Arbutus menziesii',
+    'Cornus Eddie\'s White Wonder',
+    'Sorbus aucuparia',
+    'Acer circinatum'
+]
 
 
 def save_img(inp, img, i):
@@ -61,4 +51,16 @@ def find_urls(inp, url, driver, iterate):
         save_img(inp, img, j)
 
 
-find_urls(inp, url, driver, iterate)
+user = os.getlogin()
+driver = driver = webdriver.Chrome(ChromeDriverManager().install())
+directory = '../images'
+
+for plant in BC_native_plants:
+    inp = plant
+    url = (
+        'https://www.google.com/search?q='
+        + str(inp)
+        + '&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947'
+    )
+    iterate = 20
+    find_urls(inp, url, driver, iterate)
