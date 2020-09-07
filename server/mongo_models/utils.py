52702606785cb3cff9db4e71f131f23ee5ff585a
Estella Song <estellajaysong@gmail.com>
2020-09-07 15:05:46 -0700

    update requirements

--- server/mongo_models/utils.py
+++ server/mongo_models/utils.py
@@ -1,5 +1,13 @@
 from time import time
+from PIL import Image
+import imagehash
 
 
 def timestamp_now():
     return int(time())
+
+
+hash = imagehash.average_hash(Image.open('test.jpg'))
+otherhash = imagehash.average_hash(Image.open('test_matchish.jpeg'))
+
+print(hash - otherhash)
