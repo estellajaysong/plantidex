015d5cfd53bfa40140efb82e78a4e794069c9553
Estella Song <estellajaysong@gmail.com>
2020-09-07 14:09:27 -0700

    write docstring for QuerySizeEnum

--- server/base_models/query_size_enum.py
+++ server/base_models/query_size_enum.py
@@ -2,6 +2,13 @@ from enum import Enum
 
 
 class QuerySizeEnum(str, Enum):
+    """
+    Where `length` is the larger of image `size.height` or image `size.width`,
+    \n `small`: length range = 0px - 499px
+    \n `medium`: length range = 500px - 999px
+    \n `large`: length range = 1000px -
+    """
+
     SMALL = 'small'
     MEDIUM = 'medium'
     LARGE = 'large'
